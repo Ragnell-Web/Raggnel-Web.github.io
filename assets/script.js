8c9b49df69db12060432b993cd5dfcbc15c9f83f
Ragnell-Web <60031399+Ragnell-Web@users.noreply.github.com>
2020-09-30 12:16:20 +0700

    Codingan dari Real Date pada Script.js

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,18 @@ var year = (yy < 1000) ? yy + 1900 : yy;
 var umur = year - tglUltah;
 
 document.getElementById('tgl').innerHTML = umur;
+
+let d = new Date();
+	let tgl = d.getDate();
+	
+
+	let months = ['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'];
+	let days = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
+	let h = d.getDay();
+	let b = d.getMonth();
+	let t = d.getFullYear();
+
+	document.getElementById('thisDate').innerHTML = days[h] + ', ' + tgl + ' ' + months[b] + ' ' + t;
 // let a = document.querySelector('li a');
 
 // a.addEventListener('click',function (e) {
