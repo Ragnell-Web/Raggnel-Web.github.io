3f2b22756f15bf0e76d5a8af77f4ecb6589526c6
Ragnell-Web <60031399+Ragnell-Web@users.noreply.github.com>
2020-09-30 09:44:19 +0700

    Update tanggal lahir console.log

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,13 @@ $('li a').on('click',function (e) {
   
 })
 
+var tglUltah = 2002
+var yy = date.getYear();
+var year = (yy < 1000) ? yy + 1900 : yy;
+var umur = year - tglUltah;
+
+console.log(umur);
+
 // let a = document.querySelector('li a');
 
 // a.addEventListener('click',function (e) {
